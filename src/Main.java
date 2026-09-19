341d99bf7c70d9904c4832ee9175276eaac935bd
Praneeth-Reddy-11703820 <praneethcrizz99notout@gmail.com>
2026-09-19 16:27:11 +0530

    overriding

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        int a;
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main extends test {
 
-            a = i + 1;
-            System.out.println("i = " + i);
-        }
+@Override
+    public int number(int a)
+    {
+        a=a*a;
+        return a;
+    }
+    public static void main(String[] args) {
+
+  /*
+   ********** Method Overriding ************
+
+        test test=new test();
+        int number = test.number(10);
+        System.out.println("number " + number);
+
+        test main=new Main();
+        int number1 = main.number(10);
+        System.out.println("number " + number1);
+*/
     }
 }
---
+++ src/test.java
@@ -0,0 +1,8 @@
+public class test {
+
+    public int number(int a)
+    {
+        a=a+a;
+        return a;
+    }
+}
